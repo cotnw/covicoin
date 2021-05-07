f9ff4c490a178ba91c0637671188ac900dfaa513
Anshul Saha <anshulsaha15@gmail.com>
2021-05-08 03:11:42 +0530

    session page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('index')
+});
+
+router.get('/session', (req, res) => {
+    res.render('session')
 });
 
 module.exports = router
