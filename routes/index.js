ba8647bde63bb4fbfdf74d870e6f847802354767
Anshul Saha <anshulsaha15@gmail.com>
2021-05-08 14:05:13 +0530

    session screens + leaderboard

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.render('index')
 });
 
+router.get('/leaderboard', (req, res) => {
+    res.render('leaderboard')
+});
+
 module.exports = router
--- routes/session.js
+++ routes/session.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.render('session')
 });
 
+router.get('/help', (req, res) => {
+    res.render('help')
+});
+
 module.exports = router
