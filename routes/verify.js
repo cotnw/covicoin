b302ab955c5d1688bfa489da3a84efdd6bbd3cf8
Anshul Saha <anshulsaha15@gmail.com>
2021-05-09 11:58:22 +0530

    verify

--- routes/verify.js
+++ routes/verify.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/User')
 const Session = require('../models/Session');
 const Lead = require('../models/Lead');
+const axios = require('axios')
+require('dotenv').config()
 
 router.get('/', (req, res) => {
     res.send('hello world');
@@ -44,7 +46,46 @@ router.get('/1', async(req, res) => {
     let session = await Session.findOne({ user_token: req.query.token, is_active: true })
     if (session) {
         let lead = await Lead.findOne({ session_id: session._id, user_token: req.query.token })
-            // Search on Twitter and confirm if user has commented the generated reply or not and set verify1 accordingly
+        if(lead) {
+            const tweetID = session.tweets[session.current_tweet_index].tweet_id
+            let user = await User.findOne({ user_token: req.query.token })
+            if (user) { 
+                console.log(tweetID)
+                const userID = user.user_id
+                const config = {
+                    method: 'get',
+                    url: `https://api.twitter.com/2/users/${userID}/tweets?expansions=referenced_tweets.id`,
+                    headers: {
+                        'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
+                    }
+                };
+
+                axios(config)
+                .then(function (response) {
+                    console.log(response.data.data[0])
+                    res.send("hi")
+                    // if(response.data.data[0].id == tweetID) {
+                    //     if(response.data.data[0].text.includes('This is a ‘COVICoin’ generated reply!')) {
+                    //         lead.verify1 = true
+                    //     } else {
+                    //         lead.verify1 = false
+                    //     }
+                    // } else {
+                    //     lead.verify1 = false
+                    // }
+                    // lead.save()
+                    // res.redirect(`verify/2?token=${req.query.token}`)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            } else {
+                res.sendStatus(404)
+            }
+        } else {
+            res.sendStatus(404)
+        }
+        // Search on Twitter and confirm if user has commented the generated reply or not and set verify1 accordingly
     } else {
         res.sendStatus(404)
     }
@@ -54,7 +95,18 @@ router.get('/2', async(req, res) => {
     let session = await Session.findOne({ user_token: req.query.token, is_active: true })
     if (session) {
         let lead = await Lead.findOne({ session_id: session._id, user_token: req.query.token })
-            // Search keywords in OCR Text and set verify2 to true in lead
+        let bool1 = lead.proof_ocr_text.trim().includes(lead.contact_number1)
+        let bool2 = lead.proof_ocr_text.trim().includes(lead.contact_number2)
+        if(bool1 == true || bool2 == true) {
+            lead.verify2 = true
+        } else {
+            lead.verify2 = false
+        }
+        const updatedLead = lead
+        lead.save()
+        console.log(updatedLead)
+        res.send('process completed')
+        // Search keywords in OCR Text and set verify2 to true in lead
     } else {
         res.sendStatus(404)
     }
